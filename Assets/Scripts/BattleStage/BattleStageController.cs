84ab577ef65cb9fafd1cc02b8ea308fbad16b57f
jyock97 <jyock1997@gmail.com>
2022-06-01 23:13:39 -0600

    [Battle System]  Implement Battle Stage

---
+++ Assets/Scripts/BattleStage/BattleStageController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:67e330031ea08ee3a97cee38a65a354b7736b747357943bef61cab99cd5ceb94
+size 4727
---
+++ Assets/Scripts/Creatures/CreatureController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7d68e8d4a0b7f6f14744b7b9bc060bf9c94bae011a1fa75a5a5fccaae58660d7
+size 106
