cad9c4cab97c9e978c0707f47a320edccbeb08a9
jyock97 <jyock1997@gmail.com>
2022-06-07 22:07:46 -0600

    Fix lfs

--- Assets/Scripts/Creatures/CreatureController.cs
+++ Assets/Scripts/Creatures/CreatureController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d68e8d4a0b7f6f14744b7b9bc060bf9c94bae011a1fa75a5a5fccaae58660d7
-size 106
+oid sha256:0e1d0c94273be8d6329905b0318cc9d177c2adcd08ff0958b361a5dc82f6191b
+size 104
